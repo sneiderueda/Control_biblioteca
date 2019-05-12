5868720195bdb95549773584510f9cdd053210dd
sneiderueda <sneider.rueda@gmail.com>
2019-05-12 11:31:06 -0500

    inicio del proyecto se establece master, login y sources

---
+++ login.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Base.Master" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="ControlBiblioteca.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <title>Login</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+
+    <h3>Login</h3>
+    <div>
+        <asp:Label ID="lb_usuario" runat="server" Text="Usuario"></asp:Label>
+    </div>
+    <div>
+        <asp:TextBox ID="txt_usuario" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Label ID="lb_contrasena" runat="server" Text="Contraseña"></asp:Label>
+    </div>
+    <div>
+        <asp:TextBox ID="txt_contrasena" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Button ID="btn_ingreso" runat="server" Text="Ingresar" />
+    </div>
+    </div>
+</asp:Content>
---
+++ login.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ControlBiblioteca {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Control lb_usuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lb_usuario;
+        
+        /// <summary>
+        /// Control txt_usuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_usuario;
+        
+        /// <summary>
+        /// Control lb_contrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lb_contrasena;
+        
+        /// <summary>
+        /// Control txt_contrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_contrasena;
+        
+        /// <summary>
+        /// Control btn_ingreso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_ingreso;
+    }
+}
